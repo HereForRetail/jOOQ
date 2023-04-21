ef3a036f5f5a01b7d2fabea9263609588a22977a
jacques-charnay <jch@hereforretail.io>
2023-04-21 17:59:03 +0200

    Remove all unused JOOQ modules and define only one global simplified pom

--- jOOQ-checker/src/main/java/org/jooq/checker/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * This package contains useful checkers that work with JSR 308 and the checker
- * framework.
- *
- * @author Lukas Eder
- * @see <a href="http://types.cs.washington.edu/checker-framework">http://types.
- *      cs.washington.edu/checker-framework</a>
- */
-package org.jooq.checker;
--- jOOQ-meta/src/main/java/org/jooq/meta/jaxb/package-info.java
+++
@@ -1,2 +0,0 @@
-@jakarta.xml.bind.annotation.XmlSchema(namespace = "http://www.jooq.org/xsd/jooq-codegen-3.16.5.xsd", elementFormDefault = jakarta.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.jooq.meta.jaxb;
